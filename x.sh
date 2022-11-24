d05c13faf3e0c0f82e619c4d067d2d1399ca0ecf
Lylia <71930916+dishapatel010@users.noreply.github.com>
2022-11-24 12:08:34 +0530

    Update x.sh

--- x.sh
+++ x.sh
@@ -1,7 +1,1 @@
-echo "git clone, Please Wait..."
-git clone https://github.com/xdwtf/Fheroku.git /tgvc-userbot-dev
-echo "Trying Requirements..."
-cd /tgvc-userbot-dev
-pip3 install -U -r requirements.txt
-echo "Booting bot"
 python3 main.py
