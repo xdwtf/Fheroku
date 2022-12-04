5b4279f123feac137c41f08f7f14dcb2219b4f63
Lylia <71930916+dishapatel010@users.noreply.github.com>
2022-12-04 20:26:23 +0530

    Update and rename app.py to xd.py

---
+++ xd.py
@@ -0,0 +1,10 @@
+import os
+from flask import Flask, jsonify, request 
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return jsonify({'message': 'hello world'})
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0',port=port)
